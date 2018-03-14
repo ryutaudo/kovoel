341b06cbd1d3ffeded4f96c04c8271c5dd36fb44
Ryuta Udo <ryutaudo@gmail.comls>
2018-03-14 11:29:10 +0900

    added wire frame of CreateCard component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import WebSpeechApi from '../utils/WebSpeechApi';
+// TEST IMPLEMENTATION
+// Should be deleted later
+import CreateCard from '../containers/CreateCard';
 
 class App extends Component {
+  testThis() {
+    console.log(this);
+  }
+
   render() {
-    
-    const test = new WebSpeechApi();
-/*
+    // const test = new WebSpeechApi();
     // speech a text
     // test.speech('owarimasho', 'ja-JP');
 
     // hear a text
-    test.hear('ja-JP', 
-      (text) => { test.speech(text, 'ja-JP') } ,
-      errorMessage => console.log(errorMessage));
-*/
+    // test.hear(
+    //   'ja-JP',
+    //   (text) => { test.speech(text, 'ja-JP'); },
+    //   errorMessage => console.log(errorMessage),
+    // );
+
     return (
       <div className="App">
-      Hello Gerpanese!!
-
+        Hello Gerpanese!!
+        <CreateCard />
       </div>
     );
   }
--- src/components/CreateCard.jsx
+++ src/components/CreateCard.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import WebSpeechApi from '../utils/WebSpeechApi';
 
 class CreateCard extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  // onChange() {
+
+  // }
+
   render() {
+    // const test = new WebSpeechApi();
+    // // hear a text
+    // test.hear(
+    //   'ja-JP',
+    //   (text) => { test.speech(text, 'ja-JP'); },
+    //   errorMessage => console.log(errorMessage),
+    // );
+
     return (
       <div className="CreateCard">
+        <input
+          type="text"
+          name="frontText"
+          id="frontText"
+          onChange={this.handleFrontTextChange}
+          defaultValue=""
+        />
+        <input
+          type="text"
+          name="frontText"
+          id="frontText"
+          onChange={this.handleFrontTextChange}
+          defaultValue=""
+        />
+        <button onClick={this.onClickDiscard}>DISCARD</button>
+        <button onClick={this.onClickRecord}>RECORD</button>
+        <button onClick={this.onClickSave}>SAVE</button>
       </div>
     );
   }
