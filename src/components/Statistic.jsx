99528b72fbeb353754713b2cba0d7dd91d00921f
Ryuta Udo <ryutaudo@gmail.comls>
2018-03-26 15:22:39 +0900

    able to toggle calendar and overview

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -7,8 +7,57 @@ class Statistic extends Component {
   render() {
     return (
       <div>
-        <Charts />
-        <Calendar />
+        <nav>
+          <div
+            className="nav nav-tabs"
+            id="nav-tab"
+            role="tablist"
+          >
+            <a
+              className="nav-item nav-link active"
+              id="nav-home-tab"
+              data-toggle="tab"
+              href="#nav-overview"
+              role="tab"
+              aria-controls="nav-overview"
+              aria-selected="true"
+            >
+            Overview
+            </a>
+            <a
+              className="nav-item nav-link"
+              id="nav-profile-tab"
+              data-toggle="tab"
+              href="#nav-calendar"
+              role="tab"
+              aria-controls="nav-clalendar"
+              aria-selected="false"
+            >
+            Calendar
+            </a>
+          </div>
+        </nav>
+        <div
+          className="tab-content"
+          id="nav-tabContent"
+        >
+          <div
+            className="tab-pane fade show active"
+            id="nav-overview"
+            role="tabpanel"
+            aria-labelledby="nav-overview-tab"
+          >
+            <Charts />
+          </div>
+          <div
+            className="tab-pane fade"
+            id="nav-calendar"
+            role="tabpanel"
+            aria-labelledby="nav-calendar-tab"
+          >
+            <Calendar />
+          </div>
+        </div>
       </div>
     );
   }
